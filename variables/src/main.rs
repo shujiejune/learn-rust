eb49b58c322113accee725a1557552b57e1c0093
shujiejune <jieniu.june@tutanota.com>
2025-07-15 10:01:42 -0700

    completed data type

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,34 @@
+use std::io;
+
 fn main() {
-    let mut x = 5;
+    let x = 5;
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println! {"The value of x in the inner scope is: {x}"};
+    }
     println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}");
+
+    let tup = (500, 6.4, 1);
+    let (_x, y, _z) = tup;
+    println!("The value of y is: {y}");
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enetr an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line.");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered is not a number.");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
